da1e2f8565c64258617e0bb2487ad79191446748
jbndbc <jbn@dbc.dk>
2017-04-27 08:35:53 +0200

    Added working update check to script. Renamed script to marathon_deployer.py to enable import

--- marathon_deployer.py
+++ marathon_deployer.py
@@ -38,13 +38,13 @@ class Marathon:
         if current is None:
             self._create_application(application)
         else:
-            if Marathon.is_subdict(application, current['app']):
+            if Marathon.is_update(application, current['app']):
+                self._update_application(application, current['app']['version'],
+                                         Marathon.is_scale_only_update(application, current['app']))
+            else:
                 self.logger.debug("comparison indicates that given %s causes no update of current %s", application,
                                   current['app'])
                 self._restart_application(application, current['app']['version'])
-            else:
-                self._update_application(application, current['app']['version'],
-                                         Marathon.is_scale_only_update(application, current))
         self.logger.info("deployment operation finished for %s", application['id'])
 
     def _get_application(self, application_id):
@@ -116,14 +116,12 @@ class Marathon:
             current = self._get_application(application_id)
             instances_ok = 0
             for task in current['app']['tasks']:
+                version_ok = True if scale_only else task['version'] >= application_version
                 if task['appId'].startswith(application_id) \
                         and task['state'] == 'TASK_RUNNING' \
-                        and Marathon.is_healthy(task):
-                    if not scale_only:
-                        if task['version'] >= application_version:
-                            instances_ok += 1
-                    else:
-                        instances_ok += 1
+                        and Marathon.is_healthy(task) \
+                        and version_ok:
+                    instances_ok += 1
             if instances_ok == int(application_instances):
                 break
             time.sleep(1)
@@ -132,13 +130,86 @@ class Marathon:
     @staticmethod
     def is_scale_only_update(application, current):
         if 'instances' in application:
-            if application['instances'] == current['app']['instances']:
+            if application['instances'] == current['instances']:
                 return False
         else:
             return False
         application_copy = copy.deepcopy(application)
-        application_copy['instances'] = current['app']['instances']
-        return Marathon.is_subdict(application_copy, current['app'])
+        application_copy['instances'] = current['instances']
+        return not Marathon.is_update(application_copy, current)
+
+    @staticmethod
+    def is_update(application, current_app):
+        app_copy = copy.deepcopy(application)
+        if Marathon.is_port_update(app_copy, current_app):
+            return True
+
+        if 'ports' in app_copy:
+            del app_copy['ports']
+        if 'portDefinitions' in app_copy:
+            del app_copy['portDefinitions']
+
+        current_copy = copy.deepcopy(current_app)
+        combined = Marathon.combine_dicts(current_copy, app_copy)
+        return current_app != combined
+
+    @staticmethod
+    def is_port_update(application, current_app):
+        if 'ports' in application:
+            if 'ports' not in current_app:
+                return True
+            if len(application['ports']) != len(current_app['ports']):
+                return True
+            if application['ports'] != current_app['ports']:
+                for (app_port, current_port) in zip(application['ports'], current_app['ports']):
+                    if app_port != 0 and app_port != current_port:
+                        return True
+        if 'portDefinitions' in application:
+            if 'portDefinitions' not in current_app:
+                return True
+            if len(application['portDefinitions']) != len(current_app['portDefinitions']):
+                return True
+            if application['portDefinitions'] != current_app['portDefinitions']:
+                for (app_def, current_def) in zip(application['portDefinitions'], current_app['portDefinitions']):
+                    app_def_copy = copy.deepcopy(app_def)
+                    current_def_copy = copy.deepcopy(current_def)
+                    if 'port' in app_def_copy and app_def_copy['port'] == 0:
+                        app_def_copy['port'] = current_def_copy['port']
+                    if current_def != Marathon.combine_dicts(current_def_copy, app_def_copy):
+                        return True
+        return False
+
+    @staticmethod
+    def combine_dicts(dst, src):
+        """ combines src into dst """
+
+        for key in src:
+            if key in dst:
+                if isinstance(dst[key], dict) and isinstance(src[key], dict):
+                    Marathon.combine_dicts(dst[key], src[key])
+                elif isinstance(dst[key], list) and isinstance(src[key], list):
+                    dst[key] = Marathon.combine_lists(dst[key], src[key])
+                else:
+                    dst[key] = src[key]
+            else:
+                dst[key] = src[key]
+        return dst
+
+    @staticmethod
+    def combine_lists(a_list, b_list):
+        """ combines a and b lists into new list """
+
+        if len(a_list) != len(b_list):
+            return b_list
+        combined = []
+        for (a, b) in zip(a_list, b_list):
+            if isinstance(a, dict) and isinstance(a, dict):
+                combined.append(Marathon.combine_dicts(a, b))
+            elif isinstance(a, list) and isinstance(b_list, list):
+                combined.append(Marathon.combine_lists(a, b))
+            else:
+                combined.append(b)
+        return combined
 
     @staticmethod
     def is_healthy(task):
@@ -169,10 +240,6 @@ class Marathon:
             warnings.simplefilter("ignore", exceptions.InsecureRequestWarning)
             response = requests.get(url, cookies=cookies, verify=False)
         return response
-
-    @staticmethod
-    def is_subdict(new, original):
-        return dict(original, **new) == original
 
 
 def parse_args():
---
+++ tests/test_marathon_deployer.py
@@ -0,0 +1,238 @@
+#!/usr/bin/env python3
+# Copyright Dansk Bibliotekscenter a/s. Licensed under GNU GPL v3
+# See license text at https://opensource.dbc.dk/licenses/gpl-3.0
+#
+# -*- coding: utf-8 -*-
+# -*- mode: python -*-
+
+import copy
+import json
+import os
+import unittest
+from marathon_deployer import Marathon
+
+
+class TestMarathon(unittest.TestCase):
+    def setUp(self):
+        with open(os.path.join(os.path.dirname(os.path.abspath(__file__)), 'app_response.json'), 'r') as app_response:
+            self.app_response = json.load(app_response)
+
+    def test_is_update_identical(self):
+        self.assertFalse(Marathon.is_update(self.app_response['app'], self.app_response['app']))
+
+    def test_is_update_no_change_except_defaults(self):
+        application = json.loads("""
+        {
+            "cmd": "env && python3 -m http.server $PORT0",
+            "constraints": [
+                ["net", "CLUSTER", "prod"],
+                ["hostname", "UNIQUE"]
+            ],
+            "container": {
+                "docker": {
+                    "image": "docker.dbc.dk/dbc-python3:latest"
+                },
+                "type": "DOCKER"
+            },
+            "cpus": 0.25,
+            "healthChecks": [
+                {
+                    "gracePeriodSeconds": 3,
+                    "intervalSeconds": 10,
+                    "maxConsecutiveFailures": 3,
+                    "path": "/",
+                    "portIndex": 0,
+                    "protocol": "HTTP",
+                    "timeoutSeconds": 5
+                }
+            ],
+            "id": "/dev/mesos-tools/marathon-deployer-test-app",
+            "instances": 3,
+            "mem": 50,
+            "ports": [
+                0
+            ],
+            "upgradeStrategy": {
+                "minimumHealthCapacity": 0.5,
+                "maximumOverCapacity": 0.5
+            }
+        }
+        """)
+        self.assertFalse(Marathon.is_update(application, self.app_response['app']))
+
+    def test_is_update_simple_change(self):
+        self.assertTrue(Marathon.is_update({'mem': 42}, self.app_response['app']))
+
+    def test_is_update_change_in_embedded_object(self):
+        application = json.loads("""
+        {
+            "container": {
+                "docker": {
+                    "image": "my_new_python3_image"
+                }
+            }
+        }
+        """)
+        self.assertTrue(Marathon.is_update(application, self.app_response['app']))
+
+    def test_is_update_change_in_embedded_list(self):
+        application = json.loads("""
+        {
+            "constraints": [
+                ["net", "CLUSTER", "staging"],
+                ["hostname", "UNIQUE"]
+            ]
+        }
+        """)
+        self.assertTrue(Marathon.is_update(application, self.app_response['app']))
+
+    def test_is_update_change_in_embedded_object_in_embedded_list(self):
+        application = json.loads("""
+        {
+            "healthChecks": [
+                {
+                    "gracePeriodSeconds": 3,
+                    "intervalSeconds": 10,
+                    "maxConsecutiveFailures": 3,
+                    "path": "/newpath",
+                    "portIndex": 0,
+                    "protocol": "HTTP",
+                    "timeoutSeconds": 5
+                }
+            ]            
+        }
+        """)
+        self.assertTrue(Marathon.is_update(application, self.app_response['app']))
+
+    def test_is_scale_only_update_true(self):
+        application = json.loads("""
+        {
+            "id": "/dev/mesos-tools/marathon-deployer-test-app",
+            "instances": 42
+        }
+        """)
+        self.assertTrue(Marathon.is_scale_only_update(application, self.app_response['app']))
+
+    def test_is_scale_only_update_false(self):
+        application = json.loads("""
+        {
+            "id": "/dev/mesos-tools/marathon-deployer-test-app",
+            "mem": 42,
+            "instances": 42
+        }
+        """)
+        self.assertFalse(Marathon.is_scale_only_update(application, self.app_response['app']))
+
+    def test_is_port_update_no_ports_or_port_definitions(self):
+        self.assertFalse(Marathon.is_port_update({}, self.app_response['app']))
+
+    def test_is_port_update_identical(self):
+        application = json.loads("""
+        {
+            "ports": [1, 2, 3],
+            "portDefinitions": [
+                {
+                    "port": 1,
+                    "protocol": "tcp"
+                },
+                {
+                    "port": 2,
+                    "protocol": "tcp"
+                },
+                {
+                    "port": 3,
+                    "protocol": "tcp"
+                }
+            ]
+        }
+        """)
+        self.assertFalse(Marathon.is_port_update(application, application))
+
+    def test_is_port_update_number_of_ports_differ(self):
+        application = json.loads("""
+        {
+            "ports": [1, 2, 3]
+        }
+        """)
+        current = copy.deepcopy(application)
+        current['ports'].append(4)
+        self.assertTrue(Marathon.is_port_update(application, current))
+
+    def test_is_port_update_ports_values_differ(self):
+        application = json.loads("""
+        {
+            "ports": [1, 2, 3]
+        }
+        """)
+        current = json.loads("""
+        {
+            "ports": [1, 22, 3]
+        }
+        """)
+        self.assertTrue(Marathon.is_port_update(application, current))
+
+    def test_is_port_update_ports_zero_values_are_not_compared(self):
+        application = json.loads("""
+        {
+            "ports": [1, 0, 3]
+        }
+        """)
+        current = json.loads("""
+        {
+            "ports": [1, 2, 3]
+        }
+        """)
+        self.assertFalse(Marathon.is_port_update(application, current))
+
+    def test_is_port_update_number_of_port_definitions_differ(self):
+        application = json.loads("""
+        {
+            "portDefinitions": [
+                {
+                    "port": 1,
+                    "protocol": "tcp"
+                }
+            ]
+        }
+        """)
+        current = copy.deepcopy(application)
+        current['portDefinitions'].append({'port': 2})
+        self.assertTrue(Marathon.is_port_update(application, current))
+
+    def test_is_port_update_port_definition_values_differ(self):
+        application = json.loads("""
+        {
+            "portDefinitions": [
+                {
+                    "port": 1,
+                    "protocol": "tcp"
+                },
+                {
+                    "port": 2,
+                    "protocol": "tcp"
+                }
+            ]            
+        }
+        """)
+        current = copy.deepcopy(application)
+        current['portDefinitions'][1]['port'] = 22
+        self.assertTrue(Marathon.is_port_update(application, current))
+
+    def test_is_port_update_port_definitions_zero_port_values_are_not_compared(self):
+        application = json.loads("""
+        {
+            "portDefinitions": [
+                {
+                    "port": 1,
+                    "protocol": "tcp"
+                },
+                {
+                    "port": 0,
+                    "protocol": "tcp"
+                }
+            ]            
+        }
+        """)
+        current = copy.deepcopy(application)
+        current['portDefinitions'][1]['port'] = 22
+        self.assertFalse(Marathon.is_port_update(application, current))
