fffe90e16a8f9a158360686db8c6ce485b9b19a0
Jens Stein Jørgensen <jsj@dbc.dk>
2017-10-03 15:57:57 +0200

    Add producer for config files
    config_producer is a set of methods to merge marathon json config files
    which extend each other. The extending json must contain an "extends"
    field which contains the name of the extended config file and a "changes"
    field which contains the values to be merged into the extended json.

---
+++ marathon_config_producer.py
@@ -0,0 +1,69 @@
+#!/usr/bin/env python3
+# Copyright Dansk Bibliotekscenter a/s. Licensed under GPLv3
+# See license text at https://opensource.dbc.dk/licenses/gpl-3.0
+
+import argparse
+import copy
+import json
+import os
+import sys
+
+class ConfigException(Exception):
+    pass
+
+def setup_args():
+    parser = argparse.ArgumentParser()
+    parser.add_argument("root", help="root of config directory")
+    parser.add_argument("config_file", help="marathon json file")
+    args = parser.parse_args()
+    return args
+
+def get_config_file(root_dir, config_name, exts=[".template", ".instance"]):
+    for root, _, files in os.walk(root_dir):
+        for f_path in files:
+            for ext in exts:
+                if f_path == config_name + ext:
+                    return os.path.join(root, f_path)
+    return None
+
+def get_extending_config_data(root_dir, instance_json_data):
+    if "extends" not in instance_json_data:
+        return None
+    extending_path = get_config_file(root_dir, instance_json_data["extends"])
+    if extending_path is None:
+        raise ConfigException("couldn't find template {} in root {}".format(
+            instance_json_data["extends"], root_dir))
+    with open(extending_path) as f:
+        return json.load(f)
+
+def iterate_extend_hierarchy(root_dir, config_path):
+    with open(config_path) as f:
+        json_data = json.load(f)
+        extending_data = get_extending_config_data(root_dir, json_data)
+        config_stack = []
+        config_stack.append(json_data)
+        while(extending_data is not None):
+            config_stack.append(extending_data)
+            extending_data = get_extending_config_data(root_dir, extending_data)
+        return config_stack
+
+def merge(src, dest):
+    new_dest = copy.deepcopy(dest)
+    for key in src:
+        if key in dest:
+            if isinstance(src[key], dict) and isinstance(dest[key], dict):
+                new_dest[key] = merge(src[key], dest[key])
+            else:
+                new_dest[key] = src[key]
+        else:
+            new_dest[key] = src[key]
+    return new_dest
+
+def merge_config_stack(config_stack):
+    dest = {}
+    for data in config_stack[::-1]:
+        if "changes" in data:
+            dest = merge(data["changes"], dest)
+        else:
+            dest = merge(data, dest)
+    return dest
---
+++ tests/test_marathon_config_producer.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python3
+# Copyright Dansk Bibliotekscenter a/s. Licensed under GPLv3
+# See license text at https://opensource.dbc.dk/licenses/gpl-3.0
+
+import unittest
+
+import marathon_config_producer
+
+class TestConfigProducer(unittest.TestCase):
+    def test_merge(self):
+        src = {"a": 1, "b": 3, "c": 3}
+        dest = {"a": 1, "b": 2, "d": 4}
+        expected_result = {"a": 1, "b": 3, "c": 3, "d": 4}
+        actual_result = marathon_config_producer.merge(src, dest)
+        self.assertEqual(expected_result, actual_result)
+
+    def test_merge_stack(self):
+        # the order of the stack is significant, base must be at the end
+        stack = [
+            {"changes": {"b": {"c": 4}}},
+            {"a": 1, "b": {"c": 2, "d": 3}}
+        ]
+        expected_result = {"a": 1, "b": {"c": 4, "d": 3}}
+        actual_result = marathon_config_producer.merge_config_stack(stack)
+        self.assertEqual(expected_result, actual_result)
